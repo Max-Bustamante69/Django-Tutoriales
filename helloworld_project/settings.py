8e6e77d9f161faf76724ef4eda2ec6182013f6f7
Maximiliano <mbustamang@eafit.edu.co>
2025-02-13 09:58:37 -0500

    SESION #5 - DEPENDENCIAS A Y B

--- helloworld_project/settings.py
+++ helloworld_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -100,9 +101,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
+IMAGE_STORAGE_CLASS = 'pages.utils.ImageLocalStorage'
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
+
 
 LANGUAGE_CODE = 'en-us'
 
